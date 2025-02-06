0d36db7e66250cc9ee387fa80b5643fb90aee0c6
Michael Chernysh <chermishan@gmail.com>
2025-02-07 01:09:39 +0200

    env

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,8 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
+TG_API_KEY = os.getenv("TG_API_KEY")
+DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
+OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
