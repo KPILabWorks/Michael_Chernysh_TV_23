fc633a9765a40450e9deabfffb6225f917db52bb
Michael Chernysh <chermishan@gmail.com>
2025-03-09 14:35:16 +0200

    some changes

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,6 @@ import os
 
 load_dotenv()
 
-TG_API_KEY = os.getenv("TG_API_KEY")
-DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
-OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+TG_API_KEY = "7905695823:AAGuHhdEluYq7OQ5j-OloB_HIF7okXPEniE"
+DEEPL_API_KEY = "dbfa5a84-52ae-474d-88f0-a0aec273e857:fx"
+OPEN_AI_API_KEY = "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"
